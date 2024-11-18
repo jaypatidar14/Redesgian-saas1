f3c6a600c1859849fef4f9df0357ce0e438239a8
jaypatidar14 <loopspell1@gmail.com>
2024-11-18 19:00:46 +0530

    it come

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
-// next.config.js
-module.exports = {
-  webpack(config, { isServer }) {
-    // Ensure that `cobe` works in the client-side bundle
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,  // Prevent errors related to the fs module (if needed)
-        path: false,  // Prevent issues related to path module (if needed)
-      };
-    }
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
